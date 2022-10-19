a9fb0bf8dcbb4bfaeb7b628f9f34be747990a7f2
nevin0890 <bcab19_2217@santhigiricollege.com>
2022-10-19 08:51:57 +0530

    initial commit

---
+++ src/main/java/com/nest/xyzcollege/xyzcollege/XyzcollegeApplication.java
@@ -0,0 +1,13 @@
+package com.nest.xyzcollege.xyzcollege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzcollegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzcollegeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/xyzcollege/xyzcollege/XyzcollegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.xyzcollege.xyzcollege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyzcollegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
